7c023b5a86204508ee91b72e90e9e88f0ee41b68
Dmytro Krutyi <krutiidmytro@gmail.com>
2025-09-03 19:32:13 +0200

    feat: add complete setup instructions and admin seeder with roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             TagSeeder::class,
             PostSeeder::class,
             CommentSeeder::class,
+            AdminSeeder::class,
         ]);
     }
 }
